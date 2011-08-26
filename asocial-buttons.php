696ee77418538f7db9252888ab82fbb419cac323
Jonathan Roy <jroy66@gmail.com>
2011-08-26 15:43:04 -0400

    Play with button sizes

--- asocial-buttons.php
+++ asocial-buttons.php
@@ -34,7 +34,7 @@
 				),
 				'button_count' => array(
 					'name' => 'Button Count',
-					'html' => '<iframe src="http://www.facebook.com/plugins/like.php?app_id=166922286717944&amp;href&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>'
+					'html' => '<iframe src="http://www.facebook.com/plugins/like.php?app_id=166922286717944&amp;href&amp;send=false&amp;layout=button_count&amp;width=85&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:85px; height:21px;" allowTransparency="true"></iframe>'
 				),
 				'box_count' => array(
 					'name' => 'Box Count',
