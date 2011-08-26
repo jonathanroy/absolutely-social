d61021593f773eb866231e1c1a8c4e1aad422dd4
Jonathan Roy <jroy66@gmail.com>
2011-08-25 21:08:47 -0400

    (Trying to) fix tumblr

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 		'linkedin' => array( 'name' => 'LinkedIn', 'submit-url' => 'http://www.linkedin.com/shareArticle?mini=true&url=%the_permalink%&title=%the_title%&summary=%the_excerpt%', 'method' => 'popup' ),
 		'newsvine' => array( 'name' => 'Newsvine', 'submit-url' => 'http://www.newsvine.com/_tools/seed&save?u=%the_permalink%&h=%the_title%', 'method' => 'link' ),
 		'stumbleupon' => array( 'name' => 'StumbleUpon', 'submit-url' => 'http://www.stumbleupon.com/submit?url=%the_permalink%&title=%the_title%', 'method' => 'popup' ),
-		'tumblr' => array( 'name' => 'Tumblr', 'submit-url' => 'http://www.tumblr.com/share/link?url=%the_permalink%&name=%the_title%&description=%the_excerpt%', 'method' => 'link' ),
+		'tumblr' => array( 'name' => 'Tumblr', 'submit-url' => 'http://www.tumblr.com/share/link?url=http://google.com&name=%the_title%&description=%the_excerpt%', 'method' => 'link' ),
 		'twitter' => array( 'name' => 'Twitter', 'submit-url' => 'http://twitter.com/home?status=%the_title% - %the_permalink%', 'method' => 'popup' ),
 		'yahoo-bookmarks' => array( 'name' => 'Yahoo Bookmarks', 'submit-url' => 'http://bookmarks.yahoo.com/toolbar/savebm?u=%the_permalink%&t=%the_title%', 'method' => 'popup' )
 	);
